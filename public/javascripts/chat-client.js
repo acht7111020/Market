c529932982ee31bf14ccef79ec2f57067092412a
ShaoAn Chien <sac840711@gmail.com>
2016-11-09 23:45:29 +0800

    emit message on the right chat box

--- public/javascripts/chat-client.js
+++ public/javascripts/chat-client.js
@@ -16,13 +16,26 @@ $(document).ready(function(){
       $messageInput = $('.messageInput').eq(index);
       $chat = $('.chatContent').eq(index);
       var friendsEmail = $('.friendsEmail').eq(index).html();
-      
-      socket.emit('send message', {content: $messageInput.val(), target: friendsEmail});
+      socket.emit('send message', {content: $messageInput.val(), target: friendsEmail, self: email});
       $messageInput.val('');
     });
 
-    socket.on('new message', function(data){
-      $chat.append(`<p class="messageText">${data.msg}</p>`);
+    socket.on('new message from other', function(data){
+      var $friendsEmail = $('.friendsEmail');
+      var index = 0;
+      $friendsEmail.each(function(){
+        if($(this).html() == data.target){
+          index = $friendsEmail.index(this);
+          return false;
+        }
+      });
+      var $fromFriend = $('.chatContent').eq(index);
+      $fromFriend.append(`<p class="messageText fromOther">${data.msg}</p>`);
+      $fromFriend.scrollTop($chat[0].scrollHeight);
+    });
+
+    socket.on('new message from self', function(data){
+      $chat.append(`<p class="messageText fromSelf">${data.msg}</p>`);
       $chat.scrollTop($chat[0].scrollHeight);
     });
   }
