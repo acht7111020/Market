6e79fcbab86c80e875cc59e7e8e89547f93bb44c
hellosirandy <sac840711@gmail.com>
2016-11-18 01:57:13 +0800

    working on store

---
+++ public/javascripts/store/store.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+  $('.card-image').height(parseInt($('.card-image').eq(0).width()));
+  $('#counter').height(parseInt($('.card').eq(0).height()));
+});
--- routes/store.js
+++ routes/store.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/userSchema');
 
 router.get('/', function(req, res, next) {
-
+  User.find(function(err, docs) {
+    var index = docs.map(function(item) {
+      return item.username;
+    }).indexOf(req.user.username);
+    docs.splice(index, 1);
+    res.render('store/store', {username: req.user.username, useremail: req.user.email, friends: docs, title: "Ballon"});
+  })
 });
 
 module.exports = router;
