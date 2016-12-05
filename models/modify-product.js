5db2d38bddf04561161846917ce86f3137173924
hellosirandy <sac840711@gmail.com>
2016-12-05 11:35:07 +0800

    finish connecting views and server of modify product

--- models/modify-product.js
+++ models/modify-product.js
@@ -1,22 +1,25 @@
 module.exports = function ModifyProduct() {
   var Product = require('./product-schema');
-
   this.add = function(body, files, storeID, productID) {
+    var picPaths = files.map(function(item) {return item.path;});
+    console.log(body.description);
     Product.findById(productID, function(err, product) {
       // var newProduct = {
       //   title: body.title,
       //   coverImagePath: files[0].path,
-      //   contentImagePath: files.map(function(item) {return item.path;}),
-      //   price: Number(body.price),
-      //   description: body.description
+      //   contentImagePath: [`${imagePathPrefix}windrunner-1.jpg`, `${imagePathPrefix}windrunner-2.jpg`],
+      //   price: 123,
+      //   description: body.description,
+      //   position: 3
       // };
-      var imagePathPrefix = '/images/products/'
+      var imagePathPrefix = '/images/products/';
+      var uploadPathPrefix = '/uploads/';
       var newProduct = {
-        title: 'windrunner',
-        coverImagePath: `${imagePathPrefix}windrunner-1.jpg`,
-        contentImagePath: [`${imagePathPrefix}windrunner-1.jpg`, `${imagePathPrefix}windrunner-2.jpg`],
-        price: 75,
-        description: 'Good for runners',
+        title: body.title,
+        coverImagePath: uploadPathPrefix + files[0].filename,
+        contentImagePath: files.map(function(item) {return uploadPathPrefix + item.filename;}),
+        price: body.price,
+        description: body.description,
         position: 1
       };
       if (!product) {
--- public/javascripts/store/store.js
+++ public/javascripts/store/store.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('.card-image').height(parseInt($('.card-image').eq(1).width()));
+  $('.card-image').height(parseInt($('.card-image').eq(0).width()));
   $('#counter').height(parseInt($('.card').eq(1).height()));
   $('.card-image').click(function(e){
     // console.log($(this).find('.productId').html());
