ed462db6f4e8b16ded54471fea5e4e2b5c1def7c
Blurry_fish <acht7111020@gmail.com>
2017-01-05 23:07:12 +0800

    add mouse image and event

---
+++ public/javascripts/layout/layout.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  function myMoveFunction(e){
+
+    var x = e.clientX;
+    var y = e.clientY;
+    var coor = "Coordinates: (" + x + "," + y + ")";
+    console.log(coor);
+    if (y < 64) y = 64;
+    $(".MouseDiv").css({left:x, top:y});
+  }
+  $( "body" ).mousemove(function( event ) {
+    myMoveFunction(event);
+  });
+});
