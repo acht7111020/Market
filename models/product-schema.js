eb274e0c2e59f8605f7638463d7cb6db0d0f3e51
hellosirandy <sac840711@gmail.com>
2017-01-06 02:55:34 +0800

    change product schema

--- models/product-schema.js
+++ models/product-schema.js
@@ -2,11 +2,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var schema = new Schema({
-  title: {type: String, required: true},
-  coverImagePath: {type: String, required: true},
-  contentImagePath: {type: [String], required: true},
-  price: {type: Number, required: true},
-  description: {type: String, required: true},
+  title: String,
+  coverImagePath: String,
+  contentImagePath: [String],
+  price: Number,
+  description: String,
   ownerStore: Schema.Types.ObjectId
 });
 
