254ac727a1b6f7bff0ef164cb765e3b51f573dd1
hellosirandy <sac840711@gmail.com>
2017-01-09 02:34:49 +0800

    add difficulty

--- routes/goosebumps.js
+++ routes/goosebumps.js
@@ -3,34 +3,26 @@ var router = express.Router();
 var Player = require('../models/player-schema');
 var Leaderboard = require('../models/leaderboord-schema');
 
-router.get('/leaderboard', function(req, res) {
-  Player.find(function(err, players) {
-    var leaderboard = {
-      leaderboard: players,
-    }
-    res.json(leaderboard);
-  });
-});
+// router.get('/leaderboard/:level', function(req, res) {
+//   Player.find({level: req.params.level}, function(err, players) {
+//     var leaderboard = {
+//       leaderboard: players,
+//     }
+//     res.json(leaderboard);
+//   });
+// });
 
 router.post('/leaderboard', function(req, res) {
   console.log(req.body);
-  // var newPlayer = {
-  //   name: req.body.name,
-  //   score: req.body.score
-  // }
-  // Leaderboard.find({level: req.body.level}, function(err, leaderboard) {
-  //   if (err) throw err;
-  //
-  // });
   var newPlayer = new Player({
     name: req.body.name,
     score: req.body.score,
-    level: req.body.level
+    level: req.body.level,
+    difficulty: req.body.difficulty
   });
   newPlayer.save(function(err) {
     if (err) throw err;
-    // console.log(newPlayer);
-    var findQuery = Player.find({level: req.body.level});
+    var findQuery = Player.find({level: req.body.level, difficulty: req.doby.difficulty});
     findQuery.sort('-score').exec(function(err, players) {
       var rate = players.length;
       for(var i = 0; i < players.length; i++) {
