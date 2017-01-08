2ddf25b45882dbc14c892579547057dbecdda01c
hellosirandy <sac840711@gmail.com>
2017-01-09 02:41:24 +0800

    add difficulty schema

--- models/player-schema.js
+++ models/player-schema.js
@@ -4,7 +4,8 @@ var Schema = mongoose.Schema;
 var schema = new Schema({
   name: String,
   score: Number,
-  level: Number
+  level: Number,
+  difficulty: Number
 });
 
 module.exports = mongoose.model('Player', schema);
